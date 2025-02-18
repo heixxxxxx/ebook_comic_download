9fbcf1f21adca4e6b0d8af94efeb228f752c110c
hx <zhanghongxuan@fltrp.com>
2025-02-18 15:57:54 +0800

    更新DlLsite

--- comicJs/dlLsiteBook.js
+++ comicJs/dlLsiteBook.js
@@ -1,13 +1,12 @@
 injectedScriptToPage("/modules/dlsite.js")
 class DlLsiteComic {
   constructor(webObj) {
-    //this.comicMsg 是从网站中拿到的具体内容
     this.comicMsg = { "网站": webObj.name };
-    //this.imageList 是图片列表
     this.scramble = {}
     this.imageList = []
     this.zipFlag = false
     this.imgBaseUrl = ""
+    this.key = ""
     this.transferCanvas = document.createElement("canvas")
     this.getBookInfo()
   }
@@ -19,7 +18,6 @@ class DlLsiteComic {
   }
   //下载 用户点击下载按钮时会触发的方法
   download() {
-
     this.getPage()
   }
   downloadZip() {
@@ -27,7 +25,8 @@ class DlLsiteComic {
     zip = new JSZip();
     this.getPage()
   }
-  getBookInfo() { (document.getElementById("pageInfo_hei")) {
+  getBookInfo() {
+    if (document.getElementById("pageInfo_hei")) {
       let data = JSON.parse(document.getElementById("pageInfo_hei").innerText)
       data.pages.forEach(item => {
         this.imageList.push(item.src)
@@ -35,13 +34,14 @@ class DlLsiteComic {
       this.comicMsg["标题"] = data.meta_data.title
       this.comicMsg["页数"] = data.page_count
       this.imgBaseUrl = data.imageUrl
+      this.key = data.imgKey
       this.sendMsg(1)
     } else {
-      setTimeout(() => { this.getInfo() }, 500)
+      setTimeout(() => { this.getBookInfo() }, 500)
     }
   }
   getPage(page = 0) {
-    if (page == this.comicMsg["总页数"]) {
+    if (page == this.imageList.length) {
       if (this.zipFlag) {
         zip.generateAsync({ type: "blob" })
           .then((content) => {
@@ -60,19 +60,235 @@ class DlLsiteComic {
     }
     this.sendMsg(2, { nowPage: page })
     var data_xhr = new XMLHttpRequest();
-    data_xhr._responseType = "arraybuffer";
+    data_xhr.responseType = "arraybuffer";
     data_xhr.open("GET", this.imgBaseUrl.replace("pageDataPos", this.imageList[page]), true);
     //发送请求
     // data_xhr.withCredentials = true;
     data_xhr.send();
     data_xhr.onreadystatechange = (e) => {
       if (data_xhr.readyState === 4 && data_xhr.status === 200) {
-        var blob = new Blob([data_xhr.response]);
-        console.log(URL.createObjectURL(blob))
-        // getPage(page + 1)
+        //转arraybuffer 格式
+        Qpe(data_xhr.response, {
+          key: this.key,
+          method: "xor"
+        }).then(r => {
+          if (this.zipFlag) {
+            zip.file(page < 10 ? '0' + page + ".jpg" : page + ".jpg", new Blob([r], {
+              type: "image/jpeg"
+            }));
+          } else {
+            chrome.runtime.sendMessage({
+              downloadUrl: URL.createObjectURL(new Blob([r], {
+                type: "image/jpeg"
+              })),
+              filename: page < 10 ? '0' + page + ".jpg" : page + ".jpg"
+            });
+          }
+          this.sendMsg(2, {
+            allPage: this.imageList.length,
+            nowPage: page
+          })
+          this.getPage(page + 1)
+        }
+
+        )
+
 
       }
     }
   }
 
 }
+
+
+
+const $pe = t => ({
+  type: "StartDownloadingImage",
+  payload: {
+    key: t
+  }
+})
+  , Bpe = (t, e) => ({
+    type: "UpdateProgressOfDownloadingImage",
+    payload: {
+      key: t,
+      progress: e
+    }
+  })
+  , xM = (t, e) => ({
+    type: "FinishedDownloadingImage",
+    payload: {
+      key: t,
+      data: e
+    }
+  })
+  , Upe = (t, e) => ({
+    type: "FailedOnDownloadingImage",
+    payload: {
+      key: t,
+      reason: e
+    }
+  })
+  , Hpe = t => ({
+    type: "EvictLoadedImage",
+    payload: {
+      key: t
+    }
+  })
+  , Vpe = () => (t, e) => {
+    switch (e.type) {
+      case "StartDownloadingImage":
+        return jpe(e.payload.key, t);
+      case "UpdateProgressOfDownloadingImage":
+        return Gpe(e.payload.key, e.payload.progress, t);
+      case "FinishedDownloadingImage":
+        return zpe(e.payload.key, e.payload.data, t);
+      case "FailedOnDownloadingImage":
+        return Kpe(e.payload.key, e.payload.reason, t);
+      case "EvictLoadedImage":
+        return Wpe(e.payload.key, t);
+      default:
+        return t
+    }
+  }
+  , jpe = (t, e) => ({
+    ...e,
+    images: {
+      ...e.images,
+      [t]: {
+        key: t,
+        data: void 0,
+        failed: !1,
+        failedReason: null,
+        progress: 0,
+        isLoading: !0
+      }
+    }
+  })
+  , Gpe = (t, e, n) => {
+    const r = n.images[t];
+    return r ? {
+      ...n,
+      images: {
+        ...n.images,
+        [t]: {
+          ...r,
+          progress: e
+        }
+      }
+    } : n
+  }
+  , zpe = (t, e, n) => {
+    const r = n.images[t];
+    return r ? {
+      ...n,
+      images: {
+        ...n.images,
+        [t]: {
+          ...r,
+          data: e,
+          failed: !1,
+          isLoading: !1
+        }
+      }
+    } : n
+  }
+  , Kpe = (t, e, n) => {
+    const r = n.images[t];
+    return r ? {
+      ...n,
+      images: {
+        ...n.images,
+        [t]: {
+          ...r,
+          data: void 0,
+          failed: !0,
+          failedReason: e,
+          isLoading: !1
+        }
+      }
+    } : n
+  }
+  , Wpe = (t, e) => (e.images[t],
+  {
+    ...e,
+    images: {
+      ...e.images,
+      [t]: void 0
+    }
+  })
+  , qpe = () => {
+    self.addEventListener("message", function (t) {
+      const e = t.data
+        , n = new Uint8Array(e.param.key.match(/.{1,2}/g).map(s => parseInt(s, 16)))
+        , r = new Uint8Array(e.data)
+        , i = n.length;
+      for (let s = 0; s < r.length; s++)
+        r[s] ^= n[s % i];
+      postMessage({
+        data: r,
+        session: e.session
+      })
+    }, !1)
+  }
+  , Ype = t => {
+    let e = t.toString();
+    e = e.substring(e.indexOf("{") + 1, e.lastIndexOf("}"));
+    const n = new Blob([e], {
+      type: "application/javascript"
+    });
+    return new Worker(URL.createObjectURL(n))
+  }
+  ;
+class Xpe {
+  constructor() {
+    this.worker = Ype(qpe),
+      this.callbackTable = {},
+      this.currentSession = 0,
+      this.worker.addEventListener("message", e => {
+        const n = e.data
+          , r = this.callbackTable[n.session];
+        r && (delete this.callbackTable[n.session],
+          r(n.data))
+      }
+      )
+  }
+  Decode(e, n) {
+    const r = this.currentSession++;
+    return new Promise(i => {
+      this.callbackTable[r] = i,
+        this.worker.postMessage({
+          data: e,
+          param: n,
+          session: r
+        })
+    }
+    )
+  }
+  Terminate() {
+    this.worker.terminate()
+  }
+}
+let cw;
+const Qpe = (t, e) => (cw == null && (cw = new Xpe),
+  cw.Decode(t, e))
+  , AM = t => {
+    const e = t.match(/.{1,2}/g);
+    return new Uint8Array(e.map(n => parseInt(n, 16)))
+  }
+  , Jpe = (t, e) => {
+    const { cipherText: n, iv: r } = "include_iv" in e ? {
+      cipherText: t.slice(16),
+      iv: t.slice(0, 16)
+    } : {
+      cipherText: t,
+      iv: AM(e.iv)
+    };
+    return crypto.subtle.importKey("raw", AM(e.key), "AES-CBC", !1, ["decrypt"]).then(i => crypto.subtle.decrypt({
+      name: "AES-CBC",
+      iv: r
+    }, i, n))
+  }
+
+
+
--- modules/dlsite.js
+++ modules/dlsite.js
@@ -1,6 +1,7 @@
 // 获取请求数据
 let _XMLHttpRequest = XMLHttpRequest
 let targetXml
+let target2Xml
 let p_dom = document.createElement("p")
 p_dom.style.display = "none"
 p_dom.id = "pageInfo_hei"
@@ -13,6 +14,9 @@ XMLHttpRequest = function () {
     if (src.indexOf('viewer-meta.json') != -1) {
       targetXml = myxml
       getMsg()
+    } else if (src.indexOf("decryption.key") != -1) {
+      target2Xml = myxml
+      getKey()
     } else if (imageUrl === '' && src.indexOf('Policy') != -1 && src.indexOf('Key-Pair-Id') != -1) {
       imageUrl = src
       let pagedata = imageUrl.split("?")[0].split("/")
@@ -29,8 +33,21 @@ function getMsg() {
   if ((targetXml.response || targetXml.responseText) && imageUrl) {
     let data = JSON.parse(targetXml.response || targetXml.responseText)
     data.imageUrl = imageUrl
+
+    if (p_dom.innerHTML.length && JSON.parse(p_dom.innerHTML).imgKey) {
+      data.imgKey = JSON.parse(p_dom.innerHTML).imgKey
+    }
     p_dom.innerHTML = JSON.stringify(data)
   } else {
     setTimeout(getMsg, 1000)
+  }
+}
+function getKey() {
+  if ((target2Xml.response || target2Xml.responseText)) {
+    let data = p_dom.innerHTML.length ? JSON.parse(p_dom.innerHTML) : { imgKey: target2Xml.responseText }
+    data.imgKey = target2Xml.responseText
+    p_dom.innerHTML = JSON.stringify(data)
+  } else {
+    setTimeout(getKey, 1000)
   }
 }
